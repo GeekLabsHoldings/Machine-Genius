500d41f551708689e4cc7936447c2016a15db4f1
Xcoder2000 <ahmed.aly@geeklabs.agency>
2024-04-18 17:14:16 +0200

    first commit

--- app/content-creator/page.tsx
+++ app/content-creator/page.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import TitleOfPage from '../_components/TitleOfPage/TitleOfPage'
 import CustomSelectInput from '../_components/CustomSelectInput/CustomSelectInput'
 
+
+
 const contentCreator = () => {
+
+  const selectOptions=[
+    "PST","Street Politics","Movie Myth","Investorcracy","Mega Projects","PST Canada"
+  ]
+  
   return (
     <div>
       <TitleOfPage title='Templates'/>
-      <CustomSelectInput />
+      <CustomSelectInput label="Select Content Type" options={selectOptions} />
     </div>
   )
 }
