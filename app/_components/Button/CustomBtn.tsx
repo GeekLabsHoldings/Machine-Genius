6f63692d52e49fc86359aa0ec7f06893c73f27f7
Huda-Roushdy <HodaRoshdy.Geeeklabs@gmail.com>
2024-04-21 01:13:09 +0200

    continue custom button

--- app/_components/Button/CustomBtn.tsx
+++ app/_components/Button/CustomBtn.tsx
@@ -1,15 +1,26 @@
 'use client';
 import styles from './CustumBtn.module.css';
 import { IBtn } from "@/app/interfaces/interfaces";
+import { useState } from 'react';
 
 const CustomBtn = ({word,icon,btnColor}:IBtn)=>{
+    const [hoverEffect, setHoverEffect] = useState(false);
+
+    const controlIconBg = (icon:IBtn['icon']) => {
+        if (icon) {
+            if (btnColor === "white") {
+                icon.props.fill = "#2A2B2A"; 
+            } else if (btnColor === "black") {
+                icon.props.fill = "#FFFFFB";
+            }
+        }
+        return icon
+    }
 
 return(
-    <button className = {btnColor === 'white' ? styles.whiteBtn : styles.blackBtn}>
+    <button className = {btnColor === 'white' ? styles.whiteBtn : styles.blackBtn} >
     <div className={styles.iconAndTxtBtn}>
-        {/* {icon && btnColor === 'white' ? icon.props.fill ='#2A2B2A' : icon.props.fill= '#FFFFFB'} */}
-        {/* {icon && btnColor === 'white' ? icon.props.fill = '#2A2B2A' : icon.props.fill = '#FFFFFB'} */}
-    {icon ? icon : null}
+    {icon ? controlIconBg(icon) : null}
     <span>{word}</span>
     </div>
     </button>
@@ -18,4 +29,3 @@ return(
 }
 
 export default CustomBtn;
-{/* <img src={icon.src} alt="button-icon"/> */}
