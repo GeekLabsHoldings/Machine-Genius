17a8c0310b6b36d3dc9f566198b6b4d1d6e951bf
Huda-Roushdy <HodaRoshdy.Geeeklabs@gmail.com>
2024-04-21 16:29:17 -0700

    article preview edit

--- app/content-creator/page.tsx
+++ app/content-creator/page.tsx
@@ -1,9 +1,7 @@
-
 import CustomSelectInput from "../_components/CustomSelectInput/CustomSelectInput";
 import CustomBtn from "../_components/Button/CustomBtn";
 
-import styles from './contentCreator.module.css'
-
+import styles from "./contentCreator.module.css";
 
 const options = [
   "PST",
@@ -11,40 +9,26 @@ const options = [
   "Movie Myth",
   "Investorcracy",
   "Mega Projects",
-  "PST Canada"
-]
-
-
+  "PST Canada",
+];
 
 const contentCreator = () => {
   return (
     <div className="flex flex-col h-full">
-
-      {/* <div className="flex flex-col justify-center items-center w-full h-full">
-      <div className="flex flex-col justify-center items-center w-[30vw] m-auto h-full">
-        <label className={styles.select_label}>I am writing a</label>
-        <CustomSelectInput label="Select Content Type" options={options} />
-      </div>
-
-
-      <div className="flex justify-between items-center">
-      <CustomBtn word="test" icon={ReloadIcon} btnColor="white" />
-      </div> */}
-      <div className="flex justify-end items-center">
-        <CustomBtn word="Next"  btnColor="black" href="content-creator/choose-brand" />
-      </div>
-
-      {/* <TopicColapse title="Canada Hates People" date="April 16th 2024">
-        <div className="space-y-[1.5vw]">
-          <ArticleWithChecked article="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore" />
-          <ArticleWithChecked article="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore" />
-          <ArticleWithChecked article="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore" />
-          <ArticleWithChecked article="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore" />
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="flex flex-col justify-center items-center w-[30vw] m-auto h-full">
+          <label className={styles.select_label}>I am writing a</label>
+          <CustomSelectInput label="Select Content Type" options={options} />
         </div>
-      </TopicColapse> */}
-      {/* <CustomBtn word="test" icon={ReloadIcon} btnColor="white" /> */}
 
-    {/* </div> */}
+        <div className="flex justify-end items-center">
+          <CustomBtn
+            word="Next"
+            btnColor="black"
+            href="content-creator/choose-brand"
+          />
+        </div>
+      </div>
     </div>
   );
 };
