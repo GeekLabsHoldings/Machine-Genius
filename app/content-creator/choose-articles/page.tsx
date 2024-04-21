5da947f93b7f38f1f744c7cdba344ed62676d954
Xcoder2000 <ahmed.aly@geeklabs.agency>
2024-04-21 17:46:58 +0200

    first commit

--- app/content-creator/choose-articles/page.tsx
+++ app/content-creator/choose-articles/page.tsx
@@ -1,11 +1,18 @@
+import CustomBtn from '@/app/_components/Button/CustomBtn'
 import React from 'react'
 
 const chooseArticles = () => {
-  return (
-    <div>
-      chooseArticles
-    </div>
-  )
+    return (
+        <div>
+            chooseArticles
+
+            <div className="flex justify-between">
+                <CustomBtn word={"Back"} btnColor="white" href={"/content-creator/working-on-article"} />
+                <CustomBtn word={"Next"} btnColor="black" href={"/content-creator/create-article"} />
+
+            </div>
+        </div>
+    )
 }
 
 export default chooseArticles
--- app/content-creator/create-article/page.tsx
+++ app/content-creator/create-article/page.tsx
@@ -5,38 +5,38 @@ import ArticleWithCheck from "../../_components/ArticleWithCheck/ArticleWithChec
 import ArticlePreview from "@/app/_components/ArticlePreview/ArticlePreview";
 import CustomBtn from "@/app/_components/Button/CustomBtn";
 
-const CreateArticle = ()=>{
-    const [selectedText,setSelectedText]= useState<string[]>([]);
-    const renderSelectedTxt = selectedText.map(oneTxt =>(
+const CreateArticle = () => {
+    const [selectedText, setSelectedText] = useState<string[]>([]);
+    const renderSelectedTxt = selectedText.map(oneTxt => (
         <div>
             <div className={`mb-3`}>
-           <ArticleWithCheck article={oneTxt}/>
-      </div>
-        </div>
-      ))
-
-    return(
-            <div className="flex flex-col gap-6">
-            <div className='flex justify-between gap-8'>
-                        <div className="w-7/12">
-                            <ArticlePreview withSelect={true} selectedText={selectedText} setSelectedText={setSelectedText}/>
-                        </div>
-
-                        <div className={`w-5/12 ${styles.selectionsHeader} flex flex-col gap-4`}>
-                <h2>Selections</h2>
-                
-                <div className={`${styles.selectionsParent}`}>
-                {renderSelectedTxt}
-                </div>
-                </div>
-
-                    </div>
-                    <div className="flex justify-between">
-                        <CustomBtn word={"Back"} btnColor="white" href={""}/>
-                        <CustomBtn word={"Next"} btnColor="black" href={""}/>
-
-                    </div>
+                <ArticleWithCheck article={oneTxt} />
             </div>
+        </div>
+    ))
+
+    return (
+        <div className="flex flex-col gap-6">
+            <div className='flex justify-between gap-8'>
+                <div className="w-7/12">
+                    <ArticlePreview withSelect={true} selectedText={selectedText} setSelectedText={setSelectedText} />
+                </div>
+
+                <div className={`w-5/12 ${styles.selectionsHeader} flex flex-col gap-4`}>
+                    <h2>Selections</h2>
+
+                    <div className={`${styles.selectionsParent}`}>
+                        {renderSelectedTxt}
+                    </div>
+                </div>
+
+            </div>
+            <div className="flex justify-between">
+                <CustomBtn word={"Back"} btnColor="white" href={"/content-creator/work-on-article"} />
+                <CustomBtn word={"Next"} btnColor="black" href={"/content-creator/create-article"} />
+
+            </div>
+        </div>
 
     )
 
