72ad39fab358f6b27c243f8c35629739a562e4ba
Huda-Roushdy <HodaRoshdy.Geeeklabs@gmail.com>
2024-04-22 10:45:53 -0700

    fixing bugs in create article page

--- app/_components/ArticleWithCheck/ArticleWithCheck.tsx
+++ app/_components/ArticleWithCheck/ArticleWithCheck.tsx
@@ -11,13 +11,12 @@ const ArticleWithCheck = ({ article }: IProps) => {
 
   return (
 
-    <div className={styles.article_with_check}>
+      article ? <div className={styles.article_with_check}>
       <input type="checkbox" name="check-article" id="" />
       <label className={styles.article}>
-        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore */}
         {article}
       </label>
-    </div>
+      </div> : null
 
   )
 }
