70421609f0a97592940bb7c0fe0673f826debfa7
ahmed-nabil-ali <mostafageeklabs@gmail.com>
2024-05-09 16:39:00 +0300

    video editors

--- app/content-creator/page.tsx
+++ app/content-creator/page.tsx
@@ -3,18 +3,7 @@ import React from 'react'
 const page = () => {
 
   return (
-    <div>
-      <video width="100%" height="100%" controls preload="none" autoPlay={true} >
-        <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" type="video/mp4" />
-        <track
-          src="/path/to/captions.vtt"
-          kind="subtitles"
-          srcLang="en"
-          label="English"
-        />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+<></>
   )
 }
 
---
+++ app/video-editor/layout.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React, { useState } from 'react'
+import styles from './videoCreator.module.css'
+import SideNav from '../_components/SideNav/SideNav';
+import TitleOfPage from '../_components/TitleOfPage/TitleOfPage';
+
+
+const layout = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
+  const [isSideNavOpen, setIsSideNavOpen] = useState<boolean>(false);
+  const [CurrentPage, setCurrentPage] = useState<string>('Content Creator');
+  
+  return (
+    <div className='w-full h-100vh p-0 flex'>
+      <div className={`${styles.Side_Nav_Wrapper} ${isSideNavOpen ? '' : styles.close}`}>
+        <SideNav isSideNavOpen={isSideNavOpen} setIsSideNavOpen={setIsSideNavOpen} setCurrentPage={setCurrentPage} />
+      </div>
+      <div className={styles.Page_Wrapper}>
+        <TitleOfPage title={CurrentPage} />
+        <div className='h-full'>
+        {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default layout
+ 
