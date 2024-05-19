fc1aee2b4582c345e3b3675ea25987aed9e18477
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-19 17:18:21 +0300

    Add files via upload

---
+++ app/administrative/video-database/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import VideoDatabaseTable from '@/app/_components/Administrative/02VideoDatabase/VideoDatabaseTable';
+import React from 'react';
+// import styles from './video-database.module.css';
+
+export default function page() {
+  return (
+    <VideoDatabaseTable />
+  )
+}
