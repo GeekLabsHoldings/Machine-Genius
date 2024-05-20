d76ecc386f96f36efeca8d53cb801bdb7d947d1f
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-20 15:46:25 +0300

    add new product modal

--- app/administrative/video-database/page.tsx
+++ app/administrative/video-database/page.tsx
@@ -5,12 +5,14 @@ import styles from "./video-database.module.css";
 import CustomSelectInput from "@/app/_components/CustomSelectInput/CustomSelectInput";
 import CustomBtn from "@/app/_components/Button/CustomBtn";
 import BasicModal from "@/app/_components/Modal/modal";
+import AddNewProductModal from "@/app/_components/Administrative/02VideoDatabase/AddNewProductModal";
 
 export default function page() {
   const [activeTab, setActiveTab] = React.useState<number>(1);
   const productTypeOptions: string[] = ["Snacks", "Cleaning", "Drinks"];
   const [demandOrder, setDemandOrder] = useState<boolean>(true);
   const [lastBoughtOrder, setLastBoughtOrder] = useState<boolean>(true);
+
 
   return (
     <section className={`${styles.videoDatabase}`}>
@@ -95,9 +97,9 @@ export default function page() {
           <div className="flex gap-2">
       
             {/* open modal to enable you to add account  */}
-            <BasicModal btnWord={'New Entry'} btnIcon={<svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
+            <AddNewProductModal btnWord={'New Entry'} btnIcon={<svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M4.58333 10.0833C4.58333 10.5896 4.99373 11 5.5 11C6.00628 11 6.41667 10.5896 6.41667 10.0833V6.41667H10.0833C10.5896 6.41667 11 6.00628 11 5.5C11 4.99373 10.5896 4.58333 10.0833 4.58333H6.41667V0.916667C6.41667 0.410401 6.00628 0 5.5 0C4.99373 0 4.58333 0.410401 4.58333 0.916667V4.58333H0.916667C0.41041 4.58333 0 4.99373 0 5.5C0 6.00628 0.41041 6.41667 0.916667 6.41667H4.58333V10.0833Z" fill="#FFFFFB" />
-            </svg>} btnColor={'black'} modalTitle={'Add New Product'} forWhat={'add_new_product'} />
+            </svg>} btnColor={'black'} modalTitle={'Add New Product'} />
 
 
 
