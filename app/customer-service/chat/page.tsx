7cee650fa4a5b56da2d8ce62fe330ddbf180c868
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-05-21 15:14:39 +0300

    Finishing chat side-menu

--- app/customer-service/chat/page.tsx
+++ app/customer-service/chat/page.tsx
@@ -10,7 +10,8 @@ const messages = [
   },
   {
     name: "John Doe",
-    message: "Hello, how can I help you today? lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    message:
+      "Hello, how can I help you today? lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     time: "12:00 PM",
   },
   {
@@ -54,31 +55,41 @@ function page() {
           <Dropdown title="Brand" items={["All", "Unread", "Read"]} />
         </div>
         <div
-          className={`flex flex-col border-2 [border-color:#DBDBD7] rounded-[20px] h-[50vh] overflow-hidden mt-6 shrink-0 grow p-5 ${styles.chat__chat__aside__menu}`}
+          className={`flex flex-col border-2 [border-color:#DBDBD7] rounded-[20px] h-[50vh] overflow-hidden mt-6 shrink-0 grow py-5 ${styles.chat__chat__aside__menu}`}
         >
           <input
             type="text"
             placeholder="Search"
-            className="w-full h-min border [border-color:var(--dark)] rounded-[5px] text-sm placeholder:[color:var(--dark)] py-2 px-4 "
+            className="h-min border mx-5 [border-color:var(--dark)] rounded-[5px] text-sm placeholder:[color:var(--dark)] py-2 px-4 "
           />
-          <ul className="flex flex-col max-h-[90%] overflow-scroll border-y [border-color:#DBDBD7] mt-5 w-full">
+
+          <div className="h-[1px] bg-[#DBDBD7] mt-5 mx-5"></div>
+          <ul className="flex flex-col relative max-h-[90%] overflow-y-auto [border-color:#DBDBD7] w-full">
             {messages.map((message, index) => (
               <li
-                className={`flex py-[23px] px=[27px] gap-5 cursor-pointer ${styles.chat__chat__aside__menu__item}`}
+                className={`cursor-pointer ${styles.chat__chat__aside__menu__item} group transition-colors duration-300 ease-in-out hover:[background-color:var(--dark)]`}
                 key={index}
               >
-                <div
-                  className={`[background-color:var(--dark)] flex items-center justify-center ${styles.chat__chat__aside__menu__profile}`}
-                >
-                  {/* <img src="/images/profile.png" alt="profile" /> */}
-                </div>
-                <div className="flex flex-col justify-center gap-1">
-                  <h3 className="font-bold text-xl">{message.name}</h3>
-                  <p className="text-base [color:#828282] overflow-hidden whitespace-nowrap text-ellipsis max-w-96">{truncateText(message.message, 40)}</p>
+                <div className="flex relative mx-5 gap-5 py-[23px] group-hover:border-transparent">
+                  <div
+                    className={`[background-color:var(--dark)] flex items-center justify-center ${styles.chat__chat__aside__menu__profile} group-hover:[background-color:var(--white)]`}
+                  >
+                    {/* <img src="/images/profile.png" alt="profile" /> */}
+                  </div>
+                  <div className="flex flex-col justify-center gap-1 w-[80%]">
+                    <h3 className="font-bold text-xl transition-colors duration-100">{message.name}</h3>
+                    <p className="text-base [color:#828282] overflow-hidden whitespace-nowrap text-ellipsis">
+                      {truncateText(message.message, 60)}
+                    </p>
+                  </div>
+                  <div className="absolute flex justify-center items-center right-4 top-0 bottom-0">
+                    <div className="w-3 h-3 rounded-full bg-[#E9313E]"></div>
+                  </div>
                 </div>
               </li>
             ))}
           </ul>
+          <div className="h-[1px] bg-[#DBDBD7] mx-5"></div>
         </div>
       </div>
     </div>
