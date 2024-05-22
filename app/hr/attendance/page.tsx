90cc5ba87cc17702e4621c87bbe2a87cdde20b38
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-22 10:48:37 +0300

    NotificationsCard

--- app/hr/attendance/page.tsx
+++ app/hr/attendance/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import AttendanceTable from '@/app/_components/HR/02Attendance/AttendanceTable';
+import NotificationsCard from '@/app/_components/HR/02Attendance/NotificationsCard';
 import React from 'react';
 
 
 export default function page() {
   return (
-    <AttendanceTable />
+    // <AttendanceTable />
+    <NotificationsCard />
   )
 }
