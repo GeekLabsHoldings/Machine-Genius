6f6721dd89aa6f2e113b0eef0b4134b850575f25
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-23 13:13:49 +0300

    NotificationsAttendanceGrid

--- app/_components/HR/02Attendance/NotificationsAttendanceGrid.tsx
+++ app/_components/HR/02Attendance/NotificationsAttendanceGrid.tsx
@@ -7,51 +7,115 @@ export default function NotificationsGrid() {
   return (
     <>
       <h1>NotificationsGrid</h1>
+
       {/* Notifications Container */}
-      <div className="h-[80vh] grid grid-cols-5 gap-[2vw] bg-red-400">
+      <div className="grid grid-cols-5 gap-[2vw] h-[80vh]">
         {/* First Column */}
-        <div className="relative flex flex-col bg-green-400 h-full">
-          <div><h2 className={`text-center ${styles.colTitle}`}>First Degree</h2></div>
-          <div className={`bg-orange-300 flex flex-col gap-[10px] h-[60vh] overflow-hidden overflow-y-auto py-2 ${styles.colContent}`}>
-            <NotificationsCard bgColor="bg-[#FFFFFB]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#FFFFFB]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#FFFFFB]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#FFFFFB]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#FFFFFB]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#FFFFFB]" btnText="Attendance History" />
+
+        <div className={`h-full flex flex-col`}>
+          <h2 className={`text-center ${styles.colTitle}`}>First Degree</h2>
+          <div className={`relative ${styles.colContent}`}>
+            <div
+              className={`space-y-[0.7vw] max-h-[70vh] overflow-y-auto ${styles.cardContainer}`}
+            >
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+              <NotificationsCard
+                bgColor="bg-[#FFFFFB]"
+                btnText="Attendance History"
+              />
+            </div>
           </div>
         </div>
+
         {/* Second Column */}
         <div className="relative">
           <h2 className={`text-center ${styles.colTitle}`}>Second Degree</h2>
           <div className={`flex flex-col gap-[10px] ${styles.colContent}`}>
-            <NotificationsCard bgColor="bg-[#EFE2A7]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#EFE2A7]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#EFE2A7]" btnText="Attendance History" />
+            <NotificationsCard
+              bgColor="bg-[#EFE2A7]"
+              btnText="Attendance History"
+            />
+            <NotificationsCard
+              bgColor="bg-[#EFE2A7]"
+              btnText="Attendance History"
+            />
+            <NotificationsCard
+              bgColor="bg-[#EFE2A7]"
+              btnText="Attendance History"
+            />
           </div>
         </div>
         {/* Third Column */}
         <div className="relative">
           <h2 className={`text-center ${styles.colTitle}`}>Third Degree</h2>
           <div className={`flex flex-col gap-[10px] ${styles.colContent}`}>
-            <NotificationsCard bgColor="bg-[#F9B68F]" btnText="Attendance History" />
+            <NotificationsCard
+              bgColor="bg-[#F9B68F]"
+              btnText="Attendance History"
+            />
           </div>
         </div>
         {/* Fourth Column */}
         <div className="relative">
           <h2 className={`text-center ${styles.colTitle}`}>Fourth Degree</h2>
           <div className={`flex flex-col gap-[10px] ${styles.colContent}`}>
-            <NotificationsCard bgColor="bg-[#F28389]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#F28389]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#F28389]" btnText="Attendance History" />
-            <NotificationsCard bgColor="bg-[#F28389]" btnText="Attendance History" />
+            <NotificationsCard
+              bgColor="bg-[#F28389]"
+              btnText="Attendance History"
+            />
+            <NotificationsCard
+              bgColor="bg-[#F28389]"
+              btnText="Attendance History"
+            />
+            <NotificationsCard
+              bgColor="bg-[#F28389]"
+              btnText="Attendance History"
+            />
+            <NotificationsCard
+              bgColor="bg-[#F28389]"
+              btnText="Attendance History"
+            />
           </div>
         </div>
         {/* Fifth Column */}
         <div className="relative">
           <h2 className={`text-center ${styles.colTitle}`}>Termination</h2>
           <div className={`flex flex-col gap-[10px] text-white`}>
-            <NotificationsCard bgColor="bg-[#E9313E]" btnText="Attendance History" />
+            <NotificationsCard
+              bgColor="bg-[#E9313E]"
+              btnText="Attendance History"
+            />
           </div>
         </div>
       </div>
