c3e8cab9c0a325edb6bfe7f9b9a7d52862ed600e
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-24 15:52:22 +0300

    Complaints

---
+++ app/hr/personnel/complaints/complaintDetails/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import ComplaintDetailsCard from "@/app/_components/HR/03Personnel/03Complaints/ComplaintDetailsCard";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <h1>Complaint Details</h1>
+      <ComplaintDetailsCard />
+    </>
+  );
+}
