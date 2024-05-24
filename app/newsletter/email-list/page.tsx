0961c89bb0abe268c21ec9e9bd1631e766662844
ahmed-nabil-ali <mostafageeklabs@gmail.com>
2024-05-24 15:52:31 +0300

    commit

--- app/newsletter/email-list/page.tsx
+++ app/newsletter/email-list/page.tsx
@@ -194,8 +194,6 @@ const page = () => {
         className={`custom_arrows ${className}`}
       >
         <svg
-          width="34"
-          height="34"
           viewBox="0 0 34 34"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -217,8 +215,6 @@ const page = () => {
         className={`custom_arrows ${className}`}
       >
         <svg
-          width="34"
-          height="34"
           viewBox="0 0 34 34"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
