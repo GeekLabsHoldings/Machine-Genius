bf22efb4a5a7b8291b2ecd6f2c06301891497441
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-24 16:00:47 +0300

    Complaint

--- app/hr/personnel/complaints/page.tsx
+++ app/hr/personnel/complaints/page.tsx
@@ -4,8 +4,11 @@ import React from 'react';
 
 export default function page() {
   return (
-    <div>
+    <>
+      <p className='font-light my-6'>
+      These are all the hiring requests and unfinished hiring processes, requested by team managers and approved by OP ( Operation Manager).<br />Make sure to go through every request in detail to find and hire the best candidate for the requested role!
+      </p>
       <ComplaintsTable />
-    </div>
+    </>
   )
 }
