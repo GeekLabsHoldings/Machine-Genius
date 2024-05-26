5fc222f02050f0ee220c37ae06a3cd7e278d7ad3
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-26 10:40:55 +0300

    commit

--- app/hr/on-boarding/page.tsx
+++ app/hr/on-boarding/page.tsx
@@ -115,57 +115,55 @@ export default function page() {
   }, []);
 
   return (
-    <>
+    <section>
       <div className="pageHeader">
         <h3 className="mt-[25px]">Employee Database</h3>
 
-        {/* filters options to filter and edit data in table */}
-        <div
-          className={`flex flex-col gap-[0.7vw] w-full pageHeader mt-[10px] mb-[25px]`}
-        >
-          <div className="flex justify-between items-end">
-            <div className={`w-8/12 flex items-center gap-[1.2vw]`}>
-              {/* Search */}
-              <input
-                type="search"
-                name=""
-                id=""
-                placeholder="Employee Name"
-                className="border border-[var(--dark)] rounded-[5px] py-[0.3vw] px-[1.2vw] w-1/2"
-              />
-              {/* Filter Options */}
-              <div className="flex flex-col w-1/4 gap-[0.3vw]">
-                <CustomSelectInput label="All Roles" options={rolesOptions} />
-              </div>
+        {/* Search & Filter Options & Buttons Container */}
+        <div className="flex justify-between items-end w-full mt-[10px] mb-[25px]">
+          <div className={`w-8/12 flex items-center gap-[1.2vw]`}>
+            {/* Search */}
+            <input
+              type="search"
+              name=""
+              id=""
+              placeholder="Employee Name"
+              className="border border-[var(--dark)] rounded-[5px] py-[0.3vw] px-[1.2vw] w-1/2"
+            />
+            {/* Filter Options */}
+            <div className="flex flex-col w-1/4 gap-[0.3vw]">
+              <CustomSelectInput label="All Roles" options={rolesOptions} />
             </div>
+          </div>
 
-            {/* BUTTONS Container */}
-            <div className="flex items-center gap-[2vw]">
-              <div className="">
-                <CustomBtn
-                  word="New Entry"
-                  icon={addIcon}
-                  btnColor="black"
-                  paddingVal="py-[0.5vw] px-[1vw]"
-                />
+          {/* BUTTONS Container */}
+          <div className="flex items-center gap-[2vw]">
+            {/* New Entry Button */}
+            <div className="">
+              <CustomBtn
+                word="New Entry"
+                icon={addIcon}
+                btnColor="black"
+                paddingVal="py-[0.5vw] px-[1vw]"
+              />
+            </div>
+            {/* Slide Buttons */}
+            <div className="flex gap-2">
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  slideLeft();
+                }}
+              >
+                {leftArrow}
               </div>
-              <div className="flex gap-2">
-                <div
-                  className="cursor-pointer"
-                  onClick={() => {
-                    slideLeft();
-                  }}
-                >
-                  {leftArrow}
-                </div>
-                <div
-                  className="cursor-pointer"
-                  onClick={() => {
-                    slideRight();
-                  }}
-                >
-                  {rightArrow}
-                </div>
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  slideRight();
+                }}
+              >
+                {rightArrow}
               </div>
             </div>
           </div>
@@ -173,6 +171,6 @@ export default function page() {
       </div>
 
       <OnBoardingTable />
-    </>
+    </section>
   );
 }
