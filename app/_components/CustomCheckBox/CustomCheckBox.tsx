52861c84801cbfd1c7ebe7e52bb79b97c967985d
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-27 16:08:13 +0300

    commit

--- app/_components/CustomCheckBox/CustomCheckBox.tsx
+++ app/_components/CustomCheckBox/CustomCheckBox.tsx
@@ -1,20 +1,23 @@
 "use client";
-import styles from './CustomCheckBox.module.css'
+import styles from "./CustomCheckBox.module.css";
 
-type IProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement> , HTMLInputElement> &{
-    value? : string,
-    accentColor? : string
-  }
+type IProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  value?: string;
+  accentColor?: string;
+};
 
-const CustomCheckBox = (props:IProps) => {
-
+const CustomCheckBox = (props: IProps) => {
   return (
-    <input 
-    className={`${styles.custom_checkbox} bg-red-300`}
-      type="checkbox" 
+    <input
+      className={`${styles.custom_checkbox} bg-red-300`}
+      type="checkbox"
       style={{ "--module-color": props.accentColor }}
-      {...props}/>
-  )
-}
+      {...props}
+    />
+  );
+};
 
-export default CustomCheckBox
+export default CustomCheckBox;
