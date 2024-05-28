1a58fe155f5a00d96498c532941652e1398d3fb4
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-05-28 15:44:01 +0300

    Add Outreach Social media

---
+++ app/outreach/social-media-outreach/page.tsx
@@ -0,0 +1,27 @@
+import CustomBtn from "@/app/_components/Button/CustomBtn";
+import CustomSelectInput from "@/app/_components/CustomSelectInput/CustomSelectInput";
+
+const options = ["Twitter", "Facebook", "Instagram", "LinkedIn"];
+
+const page = () => {
+  return (
+    <>
+      <div className="flex flex-col h-full">
+        <div className="flex flex-col justify-center items-center w-full">
+          <div className="flex flex-col justify-center gap-12 items-center w-[30vw] min-w-[20rem] mx-auto h-[75vh] py-[1.5vw]">
+            {/* writing type select */}
+            <h2 className="text-[2.5rem] font-bold">I'm mass messaging on</h2>
+            <CustomSelectInput label="Select Brand" options={options} />
+          </div>
+
+          {/* buttons to move to last or next page */}
+          <div className="flex justify-end items-center w-full">
+            <CustomBtn word="Next" btnColor="black" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
