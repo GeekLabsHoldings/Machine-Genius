096270a756d1e70527adc790e3e42fe57bd4f236
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-05-30 13:50:23 +0300

    commit

--- app/seo/website-speed/website-speed-result/page.tsx
+++ app/seo/website-speed/website-speed-result/page.tsx
@@ -6,12 +6,12 @@ import OpportunitiesTable from "@/app/_components/SEO/06WebsiteSpeed/Opportuniti
 
 export default function page() {
   return (
-    <section className="h-[70vh] mt-[6vh]">
+    <section className="h-[70vh] mt-[4vh]">
       <div className="grid grid-cols-2 gap-[2vw]">
         {/* Overview Card */}
         <div className="flex-grow">
-          <h3 className="font-bold text-[24px]">Overview</h3>
-          <div className={styles.card + " p-4"}>
+          <h3 className="font-bold text-[24px] mb-2">Overview</h3>
+          <div className={styles.card + " p-4 grid grid-cols-2"}>
             <div className="flex flex-col items-center justify-center gap-1">
               {/* <img
                 src={"/assets/websiteSpeedIndicator.png"}
@@ -25,7 +25,55 @@ export default function page() {
                 Last Check on 21 Apr
               </span>
             </div>
-            <div></div>
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2">
+                <span className="font-bold">Website</span>
+                <span>https://machinegenius.io/signin</span>
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className="font-bold">Speed Scale</span>
+                <span className="flex items-center gap-2 text-[#ACACAC]">
+                  <svg
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="6" cy="6" r="6" fill="#5FA85B" />
+                  </svg>
+                  90 - 100 (fast)
+                </span>
+                <span className="flex items-center gap-2 text-[#ACACAC]">
+                  <svg
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="6" cy="6" r="6" fill="#E1C655" />
+                  </svg>
+                  50 - 89 (average)
+                </span>
+                <span className="flex items-center gap-2 text-[#ACACAC]">
+                  <svg
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="6" cy="6" r="6" fill="#EC6060" />
+                  </svg>
+                  0 - 49 (slow)
+                </span>
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className="font-bold">Analysis Time</span>
+                <span className="text-[#ACACAC]">21/4/20204, 1:25 PM</span>
+              </div>
+            </div>
           </div>
         </div>
         {/* Field Data Card */}
@@ -71,7 +119,9 @@ export default function page() {
         {/* Opportunities Card */}
         <div>
           <h3 className="font-bold text-[24px] mb-2">Opportunities</h3>
-          <p className="mb-2">These optimizations can speed up your page load</p>
+          <p className="mb-2">
+            These optimizations can speed up your page load
+          </p>
           <OpportunitiesTable />
         </div>
       </div>
