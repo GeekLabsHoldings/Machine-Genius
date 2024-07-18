434638b5182302a7ecab29c723c6ad50c1ebb139
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-07-18 11:21:33 +0300

    ChooseBrandPage

--- app/content-creator/create/choose-brand/ChooseBrandPage.tsx
+++ app/content-creator/create/choose-brand/ChooseBrandPage.tsx
@@ -6,54 +6,34 @@ import { useEffect, useState, useContext } from "react";
 import { useRouter } from "next/navigation";
 import { globalContext } from "@/app/_context/store";
 import LogoAndTitle from "@/app/_components/LogoAndTitle/LogoAndTitle";
-// import Radio from '@mui/material/Radio';
-// import RadioGroup from '@mui/material/RadioGroup';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import FormControl from '@mui/material/FormControl';
-// import FormLabel from '@mui/material/FormLabel';
 
 export default function ChooseBrandPage() {
   const router = useRouter();
 
   const {
+    setSelectedBrand,
     setCollectedData,
     setChoosedArticles,
     setSelectedArticle,
     setSelectedText,
     setFinalArticle,
-    collectedData,
+    setCheckGrammerResults,
     selectedBrand,
-    setSelectedBrand,
   } = useContext(globalContext);
 
   // reset all the data
   useEffect(() => {
+    setSelectedBrand("");
     setCollectedData(null);
     setChoosedArticles([]);
     setSelectedArticle(null);
     setSelectedText([]);
     setFinalArticle(null);
+    setCheckGrammerResults([]);
   }, []);
 
-  // loading state that show and hide loading
   const [IsLoading, setIsLoading] = useState(false);
   const [IsRetry, setIsRetry] = useState(false);
-  // selected option from custom select
-  const [SelectedValue, setSelectedValue] = useState<any>("");
-  // function that get select value by sending to custom select as a prop
-  const getValue = (value: string | number) => {
-    setSelectedValue(value);
-    setSelectedBrand(value);
-    if (typeof window !== "undefined") {
-      sessionStorage.setItem("selectedBrand", value.toString());
-    }
-  };
-
-  const [stockNameValue, setStockNameValue] = useState("NVDA");
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setStockNameValue((event.target as HTMLInputElement).value);
-  // };
-
   // select options
   const options = [
     "PST USA",
@@ -63,21 +43,36 @@ export default function ChooseBrandPage() {
     "Mega Projects",
   ];
 
+  const [stockNameValue, setStockNameValue] = useState("NVDA");
+  // function that get select value by sending to custom select as a prop
+  const getValue = (value: string | number) => {
+    setSelectedBrand(value);
+    if (typeof window !== "undefined") {
+      sessionStorage.setItem("selectedBrand", value.toString());
+    }
+  };
+
+  useEffect(() => {
+    console.log(`selectedBrand:`, selectedBrand);
+    // navigate to movie myth if user select movie myth option
+    // if (selectedBrand === "Movie Myth") {
+    //   router.push("/content-creator/create/movie-myth");
+    // }
+  }, [selectedBrand]);
+
+
 
   async function setCollectedDataAsync(json:any) {
     setCollectedData(json.organizedArticles);
     return Promise.resolve(); // Ensure this function is awaited properly
   }
 
-  async function getCollectedData(selectedValue: any) {
-    let brandNameValue;
+  async function getCollectedData() {
     let postBody: any = {};
-    if (selectedValue === "PST Canada") {
-      brandNameValue = "STP";
-      postBody.brandName = brandNameValue;
-    } else if (selectedValue === "Investorcracy") {
-      brandNameValue = "INV";
-      postBody.brandName = brandNameValue;
+    if (selectedBrand === "PST Canada") {
+      postBody.brandName = "STP";
+    } else if (selectedBrand === "Investorcracy") {
+      postBody.brandName = "INV";
       postBody.stockName = stockNameValue;
     }
 
@@ -94,16 +89,10 @@ export default function ChooseBrandPage() {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(postBody),
-          // cache: 'no-store',
         });
 
         if (!res.ok) {
           throw new Error("Failed to fetch data");
-        }
-
-        const contentType = res.headers.get("content-type");
-        if (!contentType || !contentType.includes("application/json")) {
-          throw new Error("Received non-JSON response");
         }
 
         json = await res.json();
@@ -127,22 +116,13 @@ export default function ChooseBrandPage() {
           JSON.stringify(json.organizedArticles)
         );
       }
-      router.push("/content-creator/create/choose-articles");
+      router.replace("/content-creator/create/choose-articles");
     } else {
       setIsRetry(true);
       // window.alert("Failed to generate content after multiple attempts");
       // router.push("/content-creator/create/choose-brand");
     }
   }
-
-  useEffect(() => {
-    console.log(SelectedValue);
-    console.log(`selectedBrand`, selectedBrand);
-    // navigate to movie myth if user select movie myth option
-    // if (SelectedValue === "Movie Myth") {
-    //   router.push("/content-creator/create/movie-myth");
-    // }
-  }, [SelectedValue]);
 
   if (IsLoading) {
     return (
@@ -157,7 +137,7 @@ export default function ChooseBrandPage() {
             btnColor="black"
             word="Retry"
             onClick={() => {
-              getCollectedData(SelectedValue);
+              getCollectedData();
               setIsRetry(false);
             }}
           />
@@ -178,23 +158,7 @@ export default function ChooseBrandPage() {
           getValue={getValue}
         />
 
-        {/* {SelectedValue === "Investorcracy" && (
-  <FormControl sx={{marginTop: "2vh"}}>
-      <RadioGroup
-        aria-labelledby="demo-controlled-radio-buttons-group"
-        name="controlled-radio-buttons-group"
-        value={stockNameValue}
-        onChange={handleChange}
-      >
-        <FormControlLabel value="NVDA" control={<Radio />} label="NVIDIA" />
-        <FormControlLabel value="AAPL" control={<Radio />} label="Apple" />
-        <FormControlLabel value="AMD" control={<Radio />} label="AMD" />
-        <FormControlLabel value="AMZN" control={<Radio />} label="Amazon" />
-        <FormControlLabel value="PLTR" control={<Radio />} label="Palantir" />
-        <FormControlLabel value="TSLA" control={<Radio />} label="Tesla" />
-      </RadioGroup>
-    </FormControl>
-)} */}
+
       </div>
 
       {/* buttons to move to last or next page */}
@@ -209,12 +173,12 @@ export default function ChooseBrandPage() {
           btnColor="black"
           onClick={() => {
             if (
-              SelectedValue !== "PST Canada" &&
-              SelectedValue !== "Investorcracy"
+              selectedBrand !== "PST Canada" &&
+              selectedBrand !== "Investorcracy"
             ) {
               // window.alert("Please select PST Canada or Investorcracy!");
             } else {
-              getCollectedData(SelectedValue);
+              getCollectedData();
             }
           }}
         />
