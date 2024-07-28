d8601405f78017449d26583fa88a9a27a52df6c3
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-07-28 16:29:09 +0300

    ShowErrorsPage

--- app/content-creator/create/show-errors/ShowErrorsPage.tsx
+++ app/content-creator/create/show-errors/ShowErrorsPage.tsx
@@ -3,31 +3,80 @@
 import CustomBtn from "@/app/_components/Button/CustomBtn";
 import styles from "./show-errors.module.css";
 import ErrorCollapse from "@/app/_components/ErrorCollapse/ErrorCollapse";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import LogoAndTitle from "@/app/_components/LogoAndTitle/LogoAndTitle";
 import SpecificChecker from "@/app/_components/SpecificChecker/SpecificChecker";
 import { globalContext } from "@/app/_context/store";
 import { useContext } from "react";
 import HighlightedContent from "@/app/_components/HighlightedContent/HighlightedContent";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { contentCreatorActions } from "@/app/_redux/contentCreator/contentCreatorSlice";
 
 export default function ShowErrorsPage() {
+  const dispatch = useDispatch();
   const [IsLoading, setIsLoading] = useState(false);
   const [selectedIssue, setSelectedIssue] = useState<any>(null);
   const [issueType, setIssueType] = useState<string>("");
   const {
-    // finalArticle,
-    // setFinalArticle,
     checkGrammerResults,
     setCheckGrammerResults,
     checkAiResults,
+    setCheckAiResults,
   } = useContext(globalContext);
 
-  const finalArticle = useSelector((state:any) => state.contentCreator.finalArticle);
+  const [startNav, setStartNav] = useState(false);
+  const finalArticle = useSelector(
+    (state: any) => state.contentCreator.finalArticle
+  );
 
-  const handleNavigate = () => {
-    setIsLoading(true);
-  };
+  const [checkStatus, setCheckStatus] = useState({
+    grammar: "waiting",
+    plagiarism: "waiting",
+    ai: "waiting",
+  });
+
+  const finalArticleRef = useRef<HTMLDivElement>(null);
+
+  async function startChecks() {
+    await checkGrammer();
+    await checkPlagiarism();
+    await checkAi();
+    return Promise.resolve();
+  }
+
+  function handleNavigate() {
+    // must be first line.
+    if (finalArticleRef.current) {
+      const finalArticleContent = finalArticleRef.current.innerText
+        .replace(/[*#]/g, "") // Remove asterisks and hash symbols
+        .replace(/[’]/g, "'") // Replace right single quotes with regular single quotes
+        .replace(/[‘]/g, "'") // Replace left single quotes with regular single quotes
+        .replace(/[“]/g, '"') // Replace left double quotes with regular double quotes
+        .replace(/[”]/g, '"') // Replace right double quotes with regular double quotes
+        .replace(/\s+/g, " ") // Normalize whitespace to a single space
+        .trim(); // Trim leading and trailing whitespace
+
+      const updatedArticle = {
+        ...finalArticle,
+        articles: [
+          {
+            ...finalArticle.articles[0],
+            content: finalArticleContent,
+          },
+        ],
+      };
+
+      dispatch(contentCreatorActions.setFinalArticle(updatedArticle));
+      console.log("+++++++++++++++++-finalArticle is updated-+++++++++++++++");
+    } else {
+      console.log(
+        "xxxxxxxxxxxxxxxxxx-finalArticleRef.current is null-xxxxxxxxxxxxxxxxxx"
+      );
+    }
+
+    setStartNav(true);
+  }
 
   useEffect(() => {
     // ===== log data =====
@@ -46,6 +95,192 @@ export default function ShowErrorsPage() {
     return [text.slice(0, start), text.slice(start, end), text.slice(end)];
   }
 
+  useEffect(() => {
+    if (startNav) {
+      setIsLoading(true);
+      console.log("finalArticle right before startChecks()", finalArticle);
+      startChecks();
+    }
+  }, [startNav]);
+
+  async function checkGrammer() {
+    const maxRetries = 2; // Define the maximum number of retries
+    let attempts = 0;
+    let json = null;
+
+    while (attempts < maxRetries) {
+      try {
+        const res = await fetch(
+          `https://backendmachinegenius.onrender.com/grammar-check`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              document: finalArticle?.articles[0]?.content,
+            }),
+            cache: "no-cache",
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        json = await res.json();
+
+        if (json) {
+          // If content is found, break the loop
+          break;
+        }
+      } catch (error) {
+        console.error("Error checkGrammer:", error);
+      } finally {
+        attempts++;
+      }
+    }
+
+    if (json) {
+      if (
+        json.grammarIssues.filter(
+          (item: any) => item.general_error_type !== "Other"
+        ).length > 0
+      ) {
+        setCheckStatus((prev) => ({ ...prev, grammar: "fail" }));
+      } else {
+        setCheckStatus((prev) => ({ ...prev, grammar: "pass" }));
+      }
+
+      console.log("checkGrammerResults1", json);
+      setCheckGrammerResults(
+        json.grammarIssues.filter(
+          (item: any) => item.general_error_type !== "Other"
+        )
+      );
+    } else {
+      setCheckStatus((prev) => ({ ...prev, grammar: "fetchError" }));
+      // window.alert("Failed to generate content after multiple attempts");
+      // router.push("/content-creator/create/choose-brand");
+    }
+  }
+
+  async function checkPlagiarism() {
+    const maxRetries = 1; // Define the maximum number of retries
+    let attempts = 0;
+    let json = null;
+
+    while (attempts < maxRetries) {
+      try {
+        const res = await fetch(
+          `https://backendmachinegenius.onrender.com/plagiarism-check`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              text: finalArticle?.articles[0]?.content,
+            }),
+            cache: "no-cache",
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        json = await res.json();
+
+        if (json) {
+          // If content is found, break the loop
+          break;
+        }
+      } catch (error) {
+        console.error("Error checkPlagiarism:", error);
+      } finally {
+        attempts++;
+      }
+    }
+
+    if (json) {
+      // todo
+      if (json) {
+        setCheckStatus((prev) => ({ ...prev, plagiarism: "fail" }));
+      } else {
+        setCheckStatus((prev) => ({ ...prev, plagiarism: "pass" }));
+      }
+      console.log("checkPlagiarismResult", json);
+    } else {
+      setCheckStatus((prev) => ({ ...prev, plagiarism: "fetchError" }));
+      // window.alert("Failed to generate content after multiple attempts");
+      // router.push("/content-creator/create/choose-brand");
+    }
+  }
+
+  async function checkAi() {
+    const maxRetries = 2; // Define the maximum number of retries
+    let attempts = 0;
+    let json = null;
+
+    while (attempts < maxRetries) {
+      try {
+        const res = await fetch(
+          `https://backendmachinegenius.onrender.com/AI-check`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              document: finalArticle?.articles[0]?.content,
+            }),
+            cache: "no-cache",
+          }
+        );
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        json = await res.json();
+
+        if (json) {
+          // If content is found, break the loop
+          break;
+        }
+      } catch (error) {
+        console.error("Error checkAi:", error);
+      } finally {
+        attempts++;
+      }
+    }
+
+    if (json) {
+      if (
+        // json.documents[0].class_probabilities.human < 0.8
+        json.documents[0].sentences.some(
+          (sentence: any) => sentence.highlight_sentence_for_ai
+        )
+      ) {
+        setCheckStatus((prev) => ({ ...prev, ai: "fail" }));
+      } else {
+        setCheckStatus((prev) => ({ ...prev, ai: "pass" }));
+      }
+      console.log("checkAiResult", json);
+      setCheckAiResults(
+        json.documents[0].sentences.filter(
+          (sentence: any) => sentence.highlight_sentence_for_ai
+        )
+      );
+    } else {
+      setCheckStatus((prev) => ({ ...prev, ai: "fetchError" }));
+      // window.alert("Failed to generate content after multiple attempts");
+      // router.push("/content-creator/create/choose-brand");
+    }
+    setStartNav(false);
+  }
+
   if (IsLoading) {
     return (
       <div className="flex flex-col justify-center items-center m-auto h-[75vh] py-[1.5vw]">
@@ -55,15 +290,44 @@ export default function ShowErrorsPage() {
             title="Genius is working on your article.."
           />
           <div className={`${styles.allCheckers} w-full`}>
-            <SpecificChecker checkStatus={"todo"} word="Grammar Checker" />
-            <SpecificChecker checkStatus={"todo"} word="Plagiarism Checker" />
-            <SpecificChecker checkStatus={"todo"} word="AI Checker" />
+            <SpecificChecker
+              checkStatus={checkStatus.grammar}
+              word="Grammar Checker"
+            />
+            <SpecificChecker
+              checkStatus={checkStatus.plagiarism}
+              word="Plagiarism Checker"
+            />
+            <SpecificChecker checkStatus={checkStatus.ai} word="AI Checker" />
           </div>
-          <CustomBtn
-            word={"Request Approval"}
-            btnColor="black"
-            href="/content-creator/create/script-approved"
-          />
+          {checkStatus.grammar !== "waiting" &&
+            checkStatus.plagiarism !== "waiting" &&
+            checkStatus.ai !== "waiting" &&
+            checkGrammerResults.length + checkAiResults.length > 0 && (
+              <CustomBtn
+                word={"Results"}
+                btnColor="black"
+                // href="/content-creator/create/show-errors/"
+                onClick={()=>{
+                  
+                  setIsLoading(false);
+                  // reset checkStatus
+                  setCheckStatus({
+                    grammar: "waiting",
+                    plagiarism: "waiting",
+                    ai: "waiting",
+                  });
+                }}
+              />
+            )}
+
+          {checkGrammerResults.length + checkAiResults.length === 0 && (
+            <CustomBtn
+              word={"Request Approval"}
+              btnColor="black"
+              href="/content-creator/create/script-approved"
+            />
+          )}
         </div>
       </div>
     );
@@ -88,6 +352,7 @@ export default function ShowErrorsPage() {
 
               <div
                 id="finalArticle"
+                ref={finalArticleRef}
                 contentEditable={true}
                 className={`${styles.articleContent}`}
                 // onInput={handleInput}
