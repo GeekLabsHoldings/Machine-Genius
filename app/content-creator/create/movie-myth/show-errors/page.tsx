9b747e8f15a0089f20729a6764ee5f08e88406dc
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-08-08 15:29:53 +0300

    refactor show-errors to component

--- app/content-creator/create/movie-myth/show-errors/page.tsx
+++ app/content-creator/create/movie-myth/show-errors/page.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 import React from "react";
 
 export default function Page() {
-  const ShowErrors = dynamic(() => import("./ShowErrorsPage"), { ssr: false });
+  const ShowErrors = dynamic(() => import("@/app/_components/ContentCreator/ShowErrorsPage"), { ssr: false });
 
   return <ShowErrors />;
 }
