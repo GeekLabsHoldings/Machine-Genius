c1693a2a0bb0201f5b0fc084407505938afac269
Abdulrahman Zaki <aazs.edu@gmail.com>
2024-08-22 23:49:09 +0300

    fix build size

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
-// import { withNextVideo } from "next-video/process";
 /**
  * @type {import('next').NextConfig}
  *
  */
 const nextConfig = {
   images: { unoptimized: true },
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.cache = false; // Disable cache in production
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
