a5f28e6c771c5e00f12832c5beec37b29dc54d88
Abdulrahman Zaki <aazs.edu@gmail.com>
2024-08-23 00:02:33 +0300

    fix build size-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 /**
  * @type {import('next').NextConfig}
  *
  */
 const nextConfig = {
   images: { unoptimized: true },
+  experimental: {
+    // Conditionally set cache option based on the environment
+    cache: !isDev ? false : undefined,
+  },
   webpack: (config, { dev }) => {
     if (!dev) {
       config.cache = false; // Disable cache in production
