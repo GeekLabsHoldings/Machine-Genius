3ff2f56b2588239e02869948755ecc9358cc8d0e
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-09-12 15:10:05 +0300

    add iframe & blockquote to contentCleaner

--- app/_utils/contentCleaner.js
+++ app/_utils/contentCleaner.js
@@ -2,8 +2,8 @@ export function contentCleaner(content) {
   if (!content) {
     return "";
   }
-  // Remove script, svg, and form tags, then, Remove empty lines
-  const regex = /<(script|svg|form)[\s\S]*?<\/\1>/gi;
+  // Remove script, svg, form, iframe, and blockquote tags, then, Remove empty lines
+  const regex = /<(script|svg|form|iframe|blockquote)[\s\S]*?<\/\1>/gi;
   let cleanedContent = content.replace(regex, "").replace(/^\s*[\r\n]/gm, "");
   return cleanedContent;
 }
