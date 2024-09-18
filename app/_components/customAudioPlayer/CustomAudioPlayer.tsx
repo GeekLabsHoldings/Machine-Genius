55f1030863557302cd3f0062563e6cc7bb3abb48
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-09-18 12:31:19 +0300

    fix CustomAudioPlayer styles

--- app/_components/customAudioPlayer/CustomAudioPlayer.tsx
+++ app/_components/customAudioPlayer/CustomAudioPlayer.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import './CustomAudioPlayer.css'
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
+"use client";
+import React from "react";
+import "./CustomAudioPlayer.css";
+import dynamic from "next/dynamic";
+
+const AudioPlayer = dynamic(() => import("react-h5-audio-player"), {
+  ssr: false,
+  loading: () => <div>Loading audio player...</div>,
+});
 
 const CustomAudioPlayer = () => {
-
-
-
-  
   return (
     <AudioPlayer
       src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
-      onPlay={e => console.log("onPlay")}
+      onPlay={(e) => console.log("onPlay")}
       autoPlayAfterSrcChange={true}
       // other props here
       className="custom_audio_player"
-      
     />
-  )
-}
+  );
+};
 
-export default CustomAudioPlayer
+export default CustomAudioPlayer;
