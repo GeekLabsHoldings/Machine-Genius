e57a2a12cd2199b2a7e7c4fc99ffbbebd88f7274
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-09-23 15:33:57 +0300

    Refactor newsletter preview page to use selectedContentTitle and generalTitles

--- app/newsletter/create/newsletter-preview/page.tsx
+++ app/newsletter/create/newsletter-preview/page.tsx
@@ -1,73 +1,37 @@
+"use client";
 import CustomBtn from "@/app/_components/Button/CustomBtn";
 import styles from "@/app/newsletter/create/newsletter-preview/newsletter-preview.module.css";
+import { use, useContext, useEffect, useState } from "react";
+import { createNewsletterContext } from "../_context/createNewsletterContext";
+import LogoAndTitle from "@/app/_components/LogoAndTitle/LogoAndTitle";
 
-const newsletter = {
-  title: "News Letter Title*",
-  sections: [
-    {
-      name: "SMALL CAPS",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-    {
-      name: "LARGE CAPS",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-    {
-      name: "MARKET NEWS",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-    {
-      name: "MEME OF THE DAY",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-    {
-      name: "LARGE CAPS",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-    {
-      name: "MARKET NEWS",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-    {
-      name: "MEME OF THE DAY",
-      news: [
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-        "#TSLA is in the lead with Q4 MAZING results!",
-      ],
-    },
-  ],
-};
+interface NewsLetter {
+  title: string;
+  articles: string[];
+}
 
 function Page() {
-  return (
-   
-    <div className="flex flex-col">
+  const { selectedContentTitle, generalTitles } = useContext(
+    createNewsletterContext
+  );
 
+  const [IsLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsLoading(false);
+  }, []);
+
+  if (IsLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center min-w-[24rem] gap-[2vw] h-[75vh] py-[1.5vw]">
+        <LogoAndTitle needTxt={false} title="Getting There..." />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
       {/* titles wrapper */}
       <div className="flex justify-end items-center flex-col h-[75vh] gap-[2vw] pb-[1.5vw]">
         {/* scripts wrapper */}
@@ -82,17 +46,17 @@ function Page() {
           className={`${styles.titles_container} flex flex-col h-full text-center overflow-hidden`}
         >
           {/* generated titles container */}
-          <h3 className="text-2xl p-4 font-bold u">{newsletter.title}</h3>
+          <h3 className="text-2xl p-4 font-bold u">{selectedContentTitle}</h3>
           <ul className="grow overflow-y-auto pb-[0.7rem]">
-            {newsletter.sections.map((section, index) => (
+            {generalTitles?.map((section: NewsLetter, index: number) => (
               <div key={index} className="">
                 <li className="text-2xl font-bold bg-[#E1C655] text-white">
                   <h4 className="text-2xl font-bold bg-[#E1C655] text-white">
-                    {section.name}
+                    {section?.title}
                   </h4>
                 </li>
-                {section.news.map((news, index) => (
-                  <li key={index} className="p-2">
+                {section?.articles?.map((news, index) => (
+                  <li key={news} className="p-2">
                     {news}
                   </li>
                 ))}
