e1f926b7302f3e0d9be6fbba60564f25b39aa821
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-10-01 11:35:59 +0300

    app\social-media\post-creation\page.tsx

--- app/social-media/post-creation/page.tsx
+++ app/social-media/post-creation/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import CustomSelectInput from "../../_components/CustomSelectInput/CustomSelectInput"; // Custom select input component
 import CustomBtn from "../../_components/Button/CustomBtn"; // Custom button component
+import { useCallback, useContext } from "react";
+import {
+  PlatformEnum,
+  socialMediaPostCreationContext,
+} from "./_context/socialMediaPostCreationContext";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 // Options for post creation type
 const platformsOptions = [
   "Facebook",
-  "Raddit",
+  "Reddit",
   "Telegram",
   "Twitter",
   "LinkedIn",
@@ -14,18 +22,31 @@ const platformsOptions = [
 
 // Post component
 const Post = () => {
+  const router = useRouter();
+  const { selectedPlatform, setSelectedPlatform } = useContext(
+    socialMediaPostCreationContext
+  );
+
+  // function that get select value by sending to custom select as a prop
+  const getValue = useCallback((value: string) => {
+    setSelectedPlatform(value.toUpperCase() as PlatformEnum);
+  }, []); // No dependencies, function reference is stable
+
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col justify-center items-center w-full">
         {/* Container for post creation */}
         <div className="flex flex-col justify-center items-center w-[30vw] min-w-[20rem] mx-auto h-[75vh] py-[1.5vw]">
           {/* Label for selecting post creation type */}
-          <label className="text-[--40px] text-center font-bold leading-normal w-full pb-[--sy-29px]">I am creating a post on</label>
+          <label className="text-[--40px] text-center font-bold leading-normal w-full pb-[--sy-29px]">
+            I am creating a post on
+          </label>
           {/* Custom select input for selecting post creation type */}
           <CustomSelectInput
             label="Select Platform"
             options={platformsOptions}
             hoverColor="hover:bg-[#E1C655]"
+            getValue={getValue}
           />
         </div>
 
@@ -34,7 +55,13 @@ const Post = () => {
           <CustomBtn
             word="Next" // Button text
             btnColor="black" // Button color
-            href="/social-media/post-creation/select-brand" // Navigation link
+            onClick={() => {
+              if (!selectedPlatform) {
+                toast.error("Please select a platform!");
+              } else {
+                router.replace("/social-media/post-creation/select-brand");
+              }
+            }}
           />
         </div>
       </div>
