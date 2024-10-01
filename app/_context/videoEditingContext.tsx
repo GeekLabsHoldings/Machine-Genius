ba2fe916734d3a267f4e1857d6b7f2e39f8d6590
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-10-01 12:26:16 +0300

    Refactor VideoEditingContextProvider component to use useSessionStorage hook for selectedContent and splitedContent

--- app/_context/videoEditingContext.tsx
+++ app/_context/videoEditingContext.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { createContext, useContext, useEffect, useState } from "react";
+import useSessionStorage from "../_hooks/useSessionStorage";
 // import { useRouter } from "next/navigation";
 // import toast from "react-hot-toast";
 // import { v4 as uuidv4 } from "uuid";
@@ -47,53 +48,14 @@ export default function VideoEditingContextProvider({
 }: {
   children: React.ReactNode;
 }) {
-  // const { authState, handleSignOut } = useContext(globalContext);
-  // const router = useRouter();
-
-  // ===== Start selectedContent =====
-  const [selectedContent, setSelectedContent] = useState<string>("");
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const selectedContentInitValue = sessionStorage.getItem(
-        "VideoEditing-selectedContent"
-      );
-      if (selectedContentInitValue !== null) {
-        setSelectedContent(selectedContentInitValue);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    sessionStorage.setItem("VideoEditing-selectedContent", selectedContent);
-    console.log("VideoEditing-selectedContent:", selectedContent);
-  }, [selectedContent]);
-  // ===== End selectedContent =====
-
-  // ===== Start splitedContent =====
-  const [splitedContent, setSplitedContent] = useState<ScriptSegment[] | null>(
-    null
+  const [selectedContent, setSelectedContent] = useSessionStorage<string>(
+    "VideoEditing-selectedContent",
+    ""
   );
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const splitedContentInitValue = sessionStorage.getItem(
-        "VideoEditing-splitedContent"
-      );
-      if (splitedContentInitValue !== null) {
-        setSplitedContent(JSON.parse(splitedContentInitValue));
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    sessionStorage.setItem(
-      "VideoEditing-splitedContent",
-      JSON.stringify(splitedContent)
-    );
-    console.log("VideoEditing-splitedContent:", splitedContent);
-  }, [splitedContent]);
-  // ===== End splitedContent =====
+  const [splitedContent, setSplitedContent] = useSessionStorage<
+    ScriptSegment[] | null
+  >("VideoEditing-splitedContent", null);
 
   // Create a context value object
   const contextValue: VideoEditingContextType = {
