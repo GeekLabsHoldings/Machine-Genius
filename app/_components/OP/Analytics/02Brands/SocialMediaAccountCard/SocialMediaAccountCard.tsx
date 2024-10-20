5961b49764c8b9a5fcead0b44009773da20ca29b
dev-abdulrahman-zaki <dev.abdulrahman.zaki@gmail.com>
2024-10-20 22:24:04 +0300

    SocialMediaAccountCard

--- app/_components/OP/Analytics/02Brands/SocialMediaAccountCard/SocialMediaAccountCard.tsx
+++ app/_components/OP/Analytics/02Brands/SocialMediaAccountCard/SocialMediaAccountCard.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 import styles from "./SocialMediaAccountCard.module.css";
-export default function SocialMediaAccountCard() {
+
+interface IProps {
+  platformName: "TELEGRAM" | "FACEBOOK" | "REDDIT";
+  brandName: string;
+  username: string;
+  followersCount: number;
+  isActive: boolean;
+  onClick?: () => void;
+}
+
+export default function SocialMediaAccountCard({
+  platformName,
+  brandName,
+  username,
+  followersCount,
+  isActive,
+  onClick,
+}: IProps) {
   return (
     <div
-      className={`${styles.card} px-[1vw] pt-[0.6vw] pb-[1vw] rounded-xl group hover:bg-[var(--dark)] hover:text-[var(--white)] cursor-pointer`}
+      className={`${
+        styles.card
+      } px-[1vw] pt-[0.6vw] pb-[1vw] rounded-xl group ${
+        isActive ? "bg-[var(--dark)] text-[var(--white)]" : ""
+      } hover:bg-[var(--dark)] hover:text-[var(--white)] cursor-pointer`}
+      onClick={onClick}
     >
-      <div className="flex justify-between items-center pb-[0.5vw] border-b-[1px] border-b-[#2A2B2A] group-hover:border-b-[var(--white)] mb-[0.5vw]">
-        <h3 className="grow font-bold text-center">Twitter</h3>
+      <div
+        className={`flex justify-between items-center pb-[0.5vw] border-b-[1px] ${
+          isActive ? "border-b-[var(--white)]" : "border-b-[#2A2B2A]"
+        } group-hover:border-b-[var(--white)] mb-[0.5vw]`}
+      >
+        <h3 className="grow font-bold text-center">{platformName}</h3>
       </div>
-      <div className="grid mx-auto w-fit grid-cols-2 ">
-        <span className="font-bold">Name:</span>
-        <span>Mega Dose</span>
-        <span className="font-bold">Username:</span>
-        <span>@MEGADOSE</span>
-        <span className="font-bold">Followers:</span>
-        <span>20.1 K</span>
+      <div className="grid mx-auto w-full grid-cols-2">
+        <span className="font-bold text-left">Name:</span>
+        <span className="text-right">{brandName}</span>
+        <span className="font-bold text-left">Username:</span>
+        <span className="text-right">{username}</span>
+        <span className="font-bold text-left">Followers:</span>
+        <span className="text-right">{followersCount}</span>
       </div>
     </div>
   );
